3de1ed980608ce5c23d10bfc115ce064c078459f
Dat Le <datle.hcmut@gmail.com>
2021-04-08 19:26:51 +0700

    Update RS485

--- loraArduino.ino
+++ loraArduino.ino
@@ -4,14 +4,30 @@
 SoftwareSerial loraSerial(RX, TX); // RX, TX
 //Sensor
 #include "DHT.h"
-#define DHTPIN 2
+#define DHTPIN 10
 #define DHTTYPE DHT22 
 DHT dht(DHTPIN, DHTTYPE);
 //TIMER
 long clkTime = 0;
+//RS485
+#include <ModbusMaster.h>   //Modbus Lib
+#define DE  3 // DE -> 3
+#define RE  2 // RE -> 2
+ModbusMaster node;   
 
+//RELAY
 int relayDV1 = 8;
 int relayDV2 = 9;
+
+//FUNCTION MODBUS
+void preTransmission(){
+  digitalWrite(RE, 1);         
+  digitalWrite(DE, 1);
+}
+void postTransmission(){
+  digitalWrite(RE, 0);
+  digitalWrite(DE, 0);
+}
 
 void setup() {
   Serial.begin(9600);
@@ -25,6 +41,18 @@ void setup() {
 
   //SENSOR
   dht.begin();
+  
+  //RS485 SETUP
+  pinMode(RE, OUTPUT);
+  pinMode(DE, OUTPUT);
+  digitalWrite(RE, 0);
+  digitalWrite(DE, 0);
+
+  node.begin(1, Serial);  //ID SLAVE: 1
+  node.preTransmission(preTransmission);  
+  node.postTransmission(postTransmission);
+
+  
 }
 void loop() {
   if(loraSerial.available()>0){
@@ -62,7 +90,9 @@ void loop() {
     clkTime = millis();
     
     int h = dht.readHumidity();
+    node.writeSingleRegister(0x40000,h); //SAVE HUMIDITY TO REGISTER 0x40000H
     int t = dht.readTemperature();
+    node.writeSingleRegister(0x40001,t); //SAVE TEMPERATURE TO REGISTER 0x40001H
 
     loraSerial.print(h*100 + t);
     
