4046e43b1b9fb5d84fa564d4617e916cc9c84e6a
Dat Le <datle.hcmut@gmail.com>
2021-04-22 11:04:26 +0700

    Removed control relay command

--- loraArduino.ino
+++ loraArduino.ino
@@ -32,10 +32,6 @@ float dataToFloat;
 int pH;
 int ORP;
 
-//RELAY
-int relayDV1 = 8;
-int relayDV2 = 9;
-
 //FUNCTION MODBUS
 void preTransmission(){
   digitalWrite(RE, 1);         
@@ -55,14 +51,8 @@ void serialEvent(){
 void setup() {
   Serial.begin(9600);
   loraSerial.begin(9600);
-  pinMode(relayDV1, OUTPUT);
-  pinMode(relayDV2, OUTPUT);
 
-  //OFF ALL
-  digitalWrite(relayDV1, HIGH);
-  digitalWrite(relayDV2, HIGH);
-
-  //SENSOR
+  //SENSOR DHT22
   dht.begin();
   
   //RS485 SETUP
@@ -81,35 +71,6 @@ void setup() {
   
 }
 void loop() {
-  if(loraSerial.available()>0){
-    String dataFromSender = loraSerial.readString();
-    
-    //SO SANH DU LIEU TU SENDER THUC HIEN LENH
-    if(dataFromSender.indexOf("DV1On") > 0){
-      loraSerial.print("SttDV1ON");
-      Serial.println("DEVICE 1 IS ON!");
-      //RELAYDV1 CONTROL ON
-      digitalWrite(relayDV1, LOW);
-    }
-    else if (dataFromSender.indexOf("DV1Off") > 0){
-      loraSerial.print("SttDV1OFF");
-      Serial.println("DEVICE 1 IS OFF!");
-      //RELAYDV1 CONTROL OFF
-      digitalWrite(relayDV1, HIGH);
-    }
-    else if (dataFromSender.indexOf("DV2On") > 0){
-      loraSerial.print("SttDV2ON");
-      Serial.println("DEVICE 2 IS ON!");
-      //RELAYDV2 CONTROL ON
-      digitalWrite(relayDV2, LOW);
-    }
-    else if (dataFromSender.indexOf("DV2Off") > 0){
-      loraSerial.print("SttDV2OFF");
-      Serial.println("DEVICE 2 IS OFF!");
-      //RELAYDV2 CONTROL OFF
-      digitalWrite(relayDV2, HIGH);
-    }
-  }
 
   //SENSOR
   if(millis() - clkTime > 30000){
